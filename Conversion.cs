f907e6ee09392c0599c5012700478d860fb63640
Juan Sebastian Garcia Jahn <js.garciajahn@outlook.de>
2021-05-11 15:04:29 +0200

    Projektdateien hinzufügen.

---
+++ Conversion.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace KataRomanNumerals
+{
+    public static class Conversion
+    {
+        /// <summary>Converts a Decimal Number to a Roman Numeral</summary>
+        /// <exception cref="ArgumentException">Thrown when a Number is greater than 99999</exception>
+        public static string ToRoman(int number)
+        {
+            if (number > 999999) throw new ArgumentException($"Number {number} is too big");
+            string output = string.Empty;
+            int[] digits = DigitConverter.SplitDigits(number);
+
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                int digit = digits[i];
+                output += DigitConverter.DigitToNumeral(digit, i);
+            }
+
+            return output;
+        }
+
+        /// <summary>Converts a Roman Numeral to its decimal representation</summary>
+        /// <exception cref="ArgumentException">Thrown when an illegal number is detected</exception>
+        public static int ToDecimal(string numeral)
+        {
+            int output = 0;
+            for (int i = 0; i < numeral.Length; i++)
+            {
+                int tmp = DigitConverter.NumeralToDigit(numeral[i]);
+                if (i < numeral.Length - 1)
+                {
+                    int next = DigitConverter.NumeralToDigit(numeral[i + 1]);
+                    if (next > tmp)
+                    {
+                        if (!(tmp == next / 5 || tmp == next / 10))
+                            throw new ArgumentException($"Number {numeral} is illegal!");
+
+                        output += next - tmp;
+                        i++;
+                        continue;
+                    }
+                }
+                output += tmp;
+            }
+
+            return output;
+        }
+
+
+    }
+}
---
+++ DigitConverter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KataRomanNumerals
+{
+    public static class DigitConverter
+    {
+        private static readonly char[][] R =
+        {
+            new char[]{ 'I', 'V', 'X' },
+            new char[]{ 'X', 'L', 'C' },
+            new char[]{ 'C', 'D', 'M' },
+            new char[]{ 'M', 'v', 'x' },
+            new char[]{ 'x', 'l', 'c' },
+            new char[]{ 'c', 'd' ,'m' }
+        };
+
+        /// <summary>Gets the respective Roman representation of a Digit in relation to its Row</summary>
+        /// <param name="digit">Single Digit to be converted</param>
+        /// <param name="pos">Location of the digit in the number</param>
+        /// <returns>Roman representation of the digit</returns>
+        public static string DigitToNumeral(int digit, int pos)
+        {
+            if (pos > R.Length) 
+                throw new ArgumentException("Row too big");
+
+            string output = string.Empty;
+            if (digit == 9) return $"{R[pos][0]}{R[pos][2]}";
+            if (digit == 4) return $"{R[pos][0]}{R[pos][1]}";
+            if (digit >= 5)
+            {
+                output += R[pos][1];
+                digit -= 5;
+            }
+            while (digit != 0)
+            {
+                output += R[pos][0];
+                digit--;
+            }
+            return output;
+        }
+
+        /// <summary>Gets the Numeric value of a roman Numeral</summary>
+        /// <param name="letter">A single roman Numeral</param>
+        public static int NumeralToDigit(char letter)
+        {
+            int t = 0;
+            for (int i = 0; i < R.Length; i++)
+            {
+                int p = Array.IndexOf(R[i], letter);
+                switch (p)
+                {
+                    case 0: t = 1; break;
+                    case 1: t = 5; break;
+                    case 2: t = 10; break;
+                }
+                if (i > 0) t *= (int)Math.Pow(10, i);
+
+                if (t != 0) return t;
+            }
+
+            throw new ArgumentException($"Letter \"{letter}\" not recognized");
+        }
+
+        /// <summary>Splits a decimal Number into its single digits</summary>
+        /// <returns>int[] with all the digits</returns>
+        public static int[] SplitDigits(int number)
+        {
+            List<int> output = new List<int>();
+            int remainer = number;
+            do
+            {
+                output.Add(remainer % 10);
+                remainer /= 10;
+            }
+            while (remainer != 0);
+            return output.ToArray();
+        }
+
+    }
+}
